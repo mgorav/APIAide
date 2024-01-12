ccfb2e4ee5b5a388daaa330c70d39a6ab97a0c66
Gaurav Malhotra <mgorav@gmail.com>
2024-01-12 03:08:45 +0000

    Resource Loader

--- src/main/java/com/gonnect/apiaide/tmdb/QueryLoaderService.java
+++ src/main/java/com/gonnect/apiaide/tmdb/QueryLoaderService.java
@@ -1,5 +1,6 @@
 package com.gonnect.apiaide.tmdb;
 
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -11,23 +12,24 @@ import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 @Service
+@Setter
 public class QueryLoaderService {
 
-    @Value("classpath:datasets/tmdb.json")
     private final ResourceLoader resourceLoader;
+
+    @Value("classpath:datasets/tmdb.json")
+    private Resource resource;
 
     public QueryLoaderService(ResourceLoader resourceLoader) {
         this.resourceLoader = resourceLoader;
     }
 
-    public String loadTmdbQueries() {
-        try {
-            Resource resource = resourceLoader.getResource("classpath:datasets/tmdb.json");
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
-                return reader.lines().collect(Collectors.joining(System.lineSeparator()));
-            }
+    public String loadTMDBQueries() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
+            return reader.lines().collect(Collectors.joining(System.lineSeparator()));
         } catch (IOException e) {
             throw new RuntimeException("Error loading TMDB queries from file", e);
         }
     }
+
 }
--- src/test/java/com/gonnect/apiaide/tmdb/QueryLoaderServiceTest.java
+++ src/test/java/com/gonnect/apiaide/tmdb/QueryLoaderServiceTest.java
@@ -2,6 +2,7 @@ package com.gonnect.apiaide.tmdb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -13,9 +14,10 @@ class QueryLoaderServiceTest {
         // Given
         ResourceLoader resourceLoader = new DefaultResourceLoader();
         QueryLoaderService queryLoaderService = new QueryLoaderService(resourceLoader);
+        queryLoaderService.setResource(resourceLoader.getResource("classpath:datasets/tmdb.json"));
 
         // When
-        String tmdbQueries = queryLoaderService.loadTmdbQueries();
+        String tmdbQueries = queryLoaderService.loadTMDBQueries();
 
         // Then
         assertNotNull(tmdbQueries);
